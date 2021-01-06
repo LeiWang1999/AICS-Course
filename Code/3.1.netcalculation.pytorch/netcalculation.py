f60c096a83f4328775ac683853f493a37236a1bb
Lei Wang <34334180+NjtechPrinceling@users.noreply.github.com>
2021-01-06 15:35:21 +0800

    update 3.1 code

--- Code/3.1.netcalculation.pytorch/netcalculation.py
+++ Code/3.1.netcalculation.pytorch/netcalculation.py
@@ -1,8 +1,9 @@
-import torch
 from torchvision import models
-
+from torchinfo import summary
 
 def get_parameter_number(net):
+    for target_list in expression_list:
+        pass
     total_num = sum(p.numel() for p in net.parameters())
     trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
     return {'Total': total_num, 'Trainable': trainable_num}
@@ -10,5 +11,8 @@ def get_parameter_number(net):
 
 if __name__ == '__main__':
     alexnet = models.alexnet(pretrained=False)
-    res = get_parameter_number(alexnet)
-    print(res)
+    vgg19 = models.vgg19(pretrained=False)
+    resnet152 = models.resnet152(pretrained=False)
+    summary(alexnet, input_size=(1,3,224,224))
+    summary(vgg19, input_size=(1, 3, 224, 224))
+    summary(resnet152, input_size=(1,3, 224,224))
